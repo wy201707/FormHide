2412cc70e4c39079f40aeee347d7ad6af25f1a41
wy201707 <2639571439@qq.com>
2024-06-12 00:23:57 +0800

    第二版：slot拆分

--- FormHide/src/App.vue
+++ FormHide/src/App.vue
@@ -1,50 +1,21 @@
 <template>
-  <form-hide>
-    <a-form
-      ref="formRef"
-      :model="formDataConfig"
-    >
+  <form-hide   v-slot="slotProps" :formDataConfig="formDataConfig" :conditionInitShowLength="conditionInitShowLength" >
       <a-row :gutter="[0,20]">
         <a-col
           :span="8"
-          v-for="itemID in formDataConfig.slice( 0,conditionFold ? conditionInitShowLength : formDataConfig.length)"
+          v-for="itemID in formDataConfig.slice( 0,slotProps.conditionFold ? conditionInitShowLength : formDataConfig.length)"
           :key="itemID"
         >
           <a-button style="background-color: aqua;width: 300px">{{itemID}}</a-button>
         </a-col>
       </a-row>
-      <!-- <a-row  :offset="8" :gutter="{ xs: 8, sm: 16, md: 24, lg: 32 }"> -->
-      <a-row>
-      
-        <a-col :offset="18" :span="8" >
-          {{ xs,sm,md,lg }}
-          <a style="font-size: 16px" @click="conditionFold=!conditionFold">
-            <template v-if="conditionFold">
-              <DownOutlined />
-            </template>
-            <template v-else>
-              <UpOutlined />
-            </template>
-            {{conditionFold? '展开':'折叠'}}
-          </a>
-        </a-col>
-        
-      </a-row>
-    </a-form>
-
   </form-hide>
 </template>
 
 <script setup>
-import { DownOutlined, UpOutlined } from '@ant-design/icons-vue';
 import {FormHide} from "./components/index.js";
 import { ref } from 'vue'; 
-
-const formRef = ref()
-  // formDataConfig=ref(new Array(10).fill((idx)=>idx+1))
-  const formDataConfig = ref(new Array(10).fill(null).map((_, idx) => idx + 1));
-  let conditionFold=ref(false);
-  let conditionInitShowLength=3
-
-
+const formDataConfig = ref(new Array(10).fill(null).map((_, idx) => idx + 1));
+let conditionInitShowLength=3
+let conditionFold=ref(false);
 </script>
--- FormHide/src/components/FormHide.vue
+++ FormHide/src/components/FormHide.vue
@@ -1,6 +1,40 @@
 <template>
-  <slot></slot>
+  <a-form  
+  :model="formDataConfig" >
+  <slot :conditionFold="conditionFold"></slot>
+  <a-row>
+  
+    <a-col :offset="18" :span="8" >
+      <!-- {{ xs,sm,md,lg }} -->
+      <a style="font-size: 16px" @click="conditionFold=!conditionFold">
+        <template v-if="conditionFold">
+          <DownOutlined />
+        </template>
+        <template v-else>
+          <UpOutlined />
+        </template>
+        {{conditionFold? '展开':'折叠'}}
+      </a>
+    </a-col>
+    
+  </a-row>
+</a-form>
+ 
 </template>
 <script setup>
+import { DownOutlined, UpOutlined } from '@ant-design/icons-vue';
+import { ref } from 'vue'; 
+
+const props = defineProps({
+  formDataConfig: {
+    type: Array,
+    required: true
+  },
+  conditionInitShowLength: {
+    type: Number,
+    required: true
+  }
+});
+let conditionFold=ref(false);
 
 </script>
