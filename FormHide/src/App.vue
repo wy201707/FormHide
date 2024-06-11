1c07dfb924ff55fa5bc7660abea13f875e6423bb
wy201707 <2639571439@qq.com>
2024-06-11 23:57:46 +0800

    第一版：实现功能

--- FormHide/src/App.vue
+++ FormHide/src/App.vue
@@ -1,32 +1,50 @@
 <template>
   <form-hide>
-    <a-row :gutter="[0,20]">
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">1</a-button>
-      </a-col>
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">2</a-button>
-      </a-col>
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">3</a-button>
-      </a-col>
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">4</a-button>
-      </a-col>
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">5</a-button>
-      </a-col>
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">6</a-button>
-      </a-col>
-      <a-col :span="8">
-        <a-button style="background-color: aqua;width: 300px">7</a-button>
-      </a-col>
-    </a-row>
+    <a-form
+      ref="formRef"
+      :model="formDataConfig"
+    >
+      <a-row :gutter="[0,20]">
+        <a-col
+          :span="8"
+          v-for="itemID in formDataConfig.slice( 0,conditionFold ? conditionInitShowLength : formDataConfig.length)"
+          :key="itemID"
+        >
+          <a-button style="background-color: aqua;width: 300px">{{itemID}}</a-button>
+        </a-col>
+      </a-row>
+      <!-- <a-row  :offset="8" :gutter="{ xs: 8, sm: 16, md: 24, lg: 32 }"> -->
+      <a-row>
+      
+        <a-col :offset="18" :span="8" >
+          {{ xs,sm,md,lg }}
+          <a style="font-size: 16px" @click="conditionFold=!conditionFold">
+            <template v-if="conditionFold">
+              <DownOutlined />
+            </template>
+            <template v-else>
+              <UpOutlined />
+            </template>
+            {{conditionFold? '展开':'折叠'}}
+          </a>
+        </a-col>
+        
+      </a-row>
+    </a-form>
+
   </form-hide>
 </template>
 
 <script setup>
-
+import { DownOutlined, UpOutlined } from '@ant-design/icons-vue';
 import {FormHide} from "./components/index.js";
+import { ref } from 'vue'; 
+
+const formRef = ref()
+  // formDataConfig=ref(new Array(10).fill((idx)=>idx+1))
+  const formDataConfig = ref(new Array(10).fill(null).map((_, idx) => idx + 1));
+  let conditionFold=ref(false);
+  let conditionInitShowLength=3
+
+
 </script>
